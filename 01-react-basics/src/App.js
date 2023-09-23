3201a35d4dae9f7a039b2736b8931b072b3244ff
vsouza <victor.fsouza2@gmail.com>
2023-09-23 20:28:59 -0300

    Assignment 1 - Solution

--- 01-react-basics/src/App.js
+++ 01-react-basics/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -26,11 +26,8 @@ function App() {
 
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
+      <h2>First App</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
